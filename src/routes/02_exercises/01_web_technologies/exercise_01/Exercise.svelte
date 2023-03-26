a5bb4e6edbbeba66cfaf86049adf7dceecb519d5
wenjierong1999 <jierong.wen@student.kuleuven.be>
2023-03-26 23:22:13 +0200

    exercise1

--- src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
@@ -1,7 +1,3 @@
-<div class="answer">My name is ...</div>
+<div class="answer" style="color: red;">My name is <b>Jierong Wen</b></div>
 
-<style>
-  div.answer {
-    color: rgb(2, 96, 45);
-  }
-</style>
+
