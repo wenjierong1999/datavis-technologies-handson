5faf8950c2584f8441562432b5d1ddbe6b477a97
wenjierong1999 <jierong.wen@student.kuleuven.be>
2023-04-29 17:39:45 +0200

    ex15-18

--- src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
@@ -10,12 +10,20 @@
     <g transform="translate({margin.left}, {margin.top})">
       <!--  -->
     </g>
+    <rect x={(innerWidth/2)-20} y={innerHeight/2-20} width=40 height=40 />
+    <circle cx={(innerWidth/2)-20+40} cy={innerHeight/2-20+40} r=20 />
   </svg>
   
   <style>
     svg {
       border: 1px solid black;
       border-radius: 5px;
+    }
+    rect {
+      fill: yellow;
+    }
+    circle {
+      fill: skyblue;
     }
   </style>
   
